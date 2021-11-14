cd3f89accd571cc55d4ba971e3de64626df552dc
aplowman <adam.jp@live.co.uk>
2021-11-14 18:09:42 +0000

    initial commit

---
+++ git_branching_test/cli.py
@@ -0,0 +1,20 @@
+import click
+
+from git_branching_test import __version__
+from git_branching_test import main
+
+
+@click.group()
+@click.version_option(version=__version__)
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("name")
+def hello(name):
+    main.hello(name)
+
+
+if __name__ == "__main__":
+    cli()
