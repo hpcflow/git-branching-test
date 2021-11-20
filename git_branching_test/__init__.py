9cb0036afde94c36a99c1b7614fd07aa6166f463
aplowman <adam.jp@live.co.uk>
2021-11-20 15:33:15 +0000

    feat: hohoho

--- git_branching_test/__init__.py
+++ git_branching_test/__init__.py
@@ -1,3 +1,7 @@
-from importlib_metadata import version
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version
+
 
 __version__ = version(__package__)
